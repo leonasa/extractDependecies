c88b44e43d6aba45d87b1fbf6e6faab567b3057c
Leonardo Santana <leonardo.santana@sita.aero>
2021-05-04 16:21:47 -0400

    Add code

---
+++ src/extractDependecies/Dependency.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ExtractDependencies
+{
+    internal class Dependency : IEquatable<Dependency>
+    {
+        public string Name { get; set; }
+        public string Version { get; set; }
+
+        public bool Equals(Dependency other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Name == other.Name && Version == other.Version;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Version);
+        }
+
+        public static bool operator ==(Dependency left, Dependency right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Dependency left, Dependency right)
+        {
+            return !Equals(left, right);
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Version: {Version}";
+        }
+    }
+}
---
+++ src/extractDependecies/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+namespace ExtractDependencies
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var pattern = @"<PackageReference Include=""(.+)"" Version=""(.+)""";
+            var listOfDependencies = new List<Dependency>();
+            var path = @"C:\Git\DotNet";
+            var list = Directory.GetFiles(path, "*.csproj", SearchOption.AllDirectories).ToList();
+
+            foreach (var filePath in list)
+            {
+                var allText = File.ReadAllText(filePath);
+                var matches = Regex.Matches(allText, pattern);
+                var ms = matches.Select(m => new Dependency { Name = m.Groups[1].Value, Version = m.Groups[2].Value }).ToList();
+                listOfDependencies.AddRange(ms);
+            }
+
+            var finalList = listOfDependencies.Distinct().Where(d => !d.Name.StartsWith("Sita.")).OrderBy(d=>d.Name);
+            var resultPath = @"C:\Git\DotNet\FlightDbDependecies\list.json";
+            File.WriteAllText(resultPath, JsonSerializer.Serialize(finalList));
+        }
+    }
+}
